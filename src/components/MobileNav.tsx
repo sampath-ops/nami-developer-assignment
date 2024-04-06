6f743e486a3c77dac1963e166711390052175c7b
sampath-ops <sampathkaali002@gmail.com>
2024-04-06 13:27:51 +0530

    Cursor pointer added

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -8,7 +8,7 @@ const MobileNav = () => {
     return (
         <nav className="md:hidden">
             <div className="flex items-center justify-between p-4">
-                <p className=" text-[16px] text-[#1A0A02] font-bold">Tasks of the Day</p>
+                <p className=" text-[16px] text-[#1A0A02] font-bold cursor-pointer">Tasks of the Day</p>
                 <div className="flex items-center gap-2">
                     <img src={search} alt="search" className=" w-5"  />
                     <img src={notification} alt="notification" className=" w-6" />
--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -4,7 +4,7 @@ const DesktopNav = () => {
     return (
         <nav className="hidden md:block">
             <div className="flex w-full justify-between p-8 border-b-2 ">
-                <p className="md:text-[32px] text-[#1A0A02] font-bold">ABCHotel</p>
+                <p className="md:text-[32px] text-[#1A0A02] font-bold cursor-pointer">ABCHotel</p>
                 <div className="flex gap-3">
                     <img src={notification} alt="notification" />
                     <img src={profile} alt="user-profile" />
